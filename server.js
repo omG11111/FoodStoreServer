3c98a8673dcceda96d3639769f642105014f4166
omG11111 <omgumber01@gmail.com>
2022-12-09 23:58:11 +0530

    sdsdsd

--- server.js
+++ server.js
@@ -27,10 +27,43 @@ const connectDB = async () => {
 };
 
 app.use(bodyParser());
+mailer.extend(app,{
+  from:'no-reply@example.com',
+  host:'smtp.gmail.com',
+  secureConnection:true,
+  port:465,
+  transportMethod:'SMTP',
+  auth:{
+      user:'gamestore.app.mk@gmail.com',
+      pass:'zgqfdmkkblmgiuqd'
+  }
+
+})
+app.set('email','/views');
+app.set('view engine', 'jade');
 app.use(cors());
 app.get("/", async (req, res) => {
   res.send("asdfffffffff");
 });
+app.post('/sendSignUpMail', async (req, res, next)=> {
+  let email=req.body.email;
+  
+ app.mailer.send('email', {
+     to:email, // REQUIRED. This can be a comma delimited string just like a normal email to field. 
+     subject: 'Test Email', // REQUIRED.
+     otherProperty: 'heloooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaa' // All additional properties are also passed to the template as local variables.
+   }, function (err) {
+     if (err) {
+       // handle error
+       // console.log(err);
+       res.json('There was an error sending the email')
+       // res.send('There was an error sending the email');
+       return;
+     }
+     res.json('Email Sent');
+     console.log("adaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa======>>>>>>>>>>>>>>>>>>>>>>>>.",email)
+   });
+ });
 app.use("/page",pageobj.Router)
 app.post("/any", async (req, res) => {
   // console.log("hitterdddd seccess",req.ip);
