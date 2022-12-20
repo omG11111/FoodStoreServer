19156a3dfcd31d96a1770ec617bcfcd651d816da
omG11111 <omgumber01@gmail.com>
2022-12-20 22:19:11 +0530

    sdf

--- routes/socketapi.js
+++ routes/socketapi.js
@@ -1,37 +1,70 @@
 
-// Router.get
-const express = require('express');
-const Router=express.Router();
-const app2 = express();
-const http = require('http');
-const server = http.createServer(app2);
-const { Server } = require("socket.io");
-const io = new Server(server);
-// const io = new Server(server,{cors: {
-//   origins: ['http://localhost:4200']
-// }});
-const socketport =process.env.SOCKET_PORT || 3020;
-const path =require("path");
-//or
-//io=require("socket.io")(http)
+// // Router.get
+// const express = require('express');
+// const Router=express.Router();
+// const app2 = express();
+// const http = require('http');
+// const server = http.createServer(app2);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+// // const io = new Server(server,{cors: {
+// //   origins: ['http://localhost:4200']
+// // }});
+// const socketport =process.env.SOCKET_PORT || 3020;
+// const path =require("path");
+// //or
+// //io=require("socket.io")(http)
 
-app2.get('/', (req, res) => {
-    console.log("asdasd");
-    console.log(path.join(__dirname, '../index.html'));
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+// app2.get('/', (req, res) => {
+//     console.log("asdasd");
+//     console.log(path.join(__dirname, '../index.html'));
+//   res.sendFile(path.join(__dirname, '../index.html'));
+// });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect',()=>{
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect',()=>{
 
-    console.log('A user disconnected');
-  })
-});
+//     console.log('A user disconnected');
+//   })
+// });
 
-server.listen(socketport, () => {
-  console.log(`listening on *:${socketport}`);
-});
+// server.listen(socketport, () => {
+//   console.log(`listening on *:${socketport}`);
+// });
+// module.exports=Router
 
+// // Router.get
+// const express = require('express');
+// const Router=express.Router();
+// const app2 = express();
+// const http = require('http');
+// const server = http.createServer(app2);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+// // const io = new Server(server,{cors: {
+// //   origins: ['http://localhost:4200']
+// // }});
+// const socketport =process.env.SOCKET_PORT || 3020;
+// const path =require("path");
+// //or
+// //io=require("socket.io")(http)
 
-module.exports=Router
+// app2.get('/', (req, res) => {
+//     console.log("asdasd");
+//     console.log(path.join(__dirname, '../index.html'));
+//   res.sendFile(path.join(__dirname, '../index.html'));
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect',()=>{
+
+//     console.log('A user disconnected');
+//   })
+// });
+
+// server.listen(socketport, () => {
+//   console.log(`listening on *:${socketport}`);
+// });
+// module.exports=Router
