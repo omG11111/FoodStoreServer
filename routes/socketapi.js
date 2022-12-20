0a705f52b5e744bd6e039f44f7729eef1f1b7655
omG11111 <omgumber01@gmail.com>
2022-12-20 22:46:54 +0530

    sdsd

--- routes/socketapi.js
+++ routes/socketapi.js
@@ -2,7 +2,7 @@
 // // Router.get
 // const express = require('express');
 // const Router=express.Router();
-// const app2 = express();
+// // const app2 = express();
 // const http = require('http');
 // const server = http.createServer(app2);
 // const { Server } = require("socket.io");
@@ -10,61 +10,68 @@
 // // const io = new Server(server,{cors: {
 // //   origins: ['http://localhost:4200']
 // // }});
-// const socketport =process.env.SOCKET_PORT || 3020;
+// // const socketport =process.env.SOCKET_PORT || 3020;
 // const path =require("path");
 // //or
 // //io=require("socket.io")(http)
 
-// app2.get('/', (req, res) => {
+// Router.get('/', (req, res) => {
 //     console.log("asdasd");
 //     console.log(path.join(__dirname, '../index.html'));
 //   res.sendFile(path.join(__dirname, '../index.html'));
 // });
 
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('disconnect',()=>{
 
-//     console.log('A user disconnected');
-//   })
-// });
 
-// server.listen(socketport, () => {
-//   console.log(`listening on *:${socketport}`);
-// });
+// // server.listen(socketport, () => {
+// //   console.log(`listening on *:${socketport}`);
+// // });
 // module.exports=Router
 
-// // Router.get
-// const express = require('express');
-// const Router=express.Router();
-// const app2 = express();
-// const http = require('http');
-// const server = http.createServer(app2);
-// const { Server } = require("socket.io");
-// const io = new Server(server);
-// // const io = new Server(server,{cors: {
-// //   origins: ['http://localhost:4200']
-// // }});
-// const socketport =process.env.SOCKET_PORT || 3020;
-// const path =require("path");
-// //or
-// //io=require("socket.io")(http)
 
-// app2.get('/', (req, res) => {
-//     console.log("asdasd");
-//     console.log(path.join(__dirname, '../index.html'));
-//   res.sendFile(path.join(__dirname, '../index.html'));
-// });
 
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-//   socket.on('disconnect',()=>{
 
-//     console.log('A user disconnected');
-//   })
-// });
 
-// server.listen(socketport, () => {
-//   console.log(`listening on *:${socketport}`);
-// });
-// module.exports=Router
+
+
+
+
+
+
+
+
+
+// Router.get
+const express = require('express');
+const Router=express.Router();
+const app2 = express();
+const http = require('http');
+const server = http.createServer(app2);
+const { Server } = require("socket.io");
+const io = new Server(server);
+// const io = new Server(server,{cors: {
+//   origins: ['http://localhost:4200']
+// }});
+const socketport =process.env.SOCKET_PORT || 3020;
+const path =require("path");
+//or
+//io=require("socket.io")(http)
+
+app2.get('/', (req, res) => {
+    console.log("asdasd");
+    console.log(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect',()=>{
+
+    console.log('A user disconnected');
+  })
+});
+
+server.listen(socketport, () => {
+  console.log(`listening on *:${socketport}`);
+});
+module.exports=Router
--- server.js
+++ server.js
@@ -1,3 +1,159 @@
+// // const { MongoClient } = require("mongodb");
+// require("./routes/socketapi");
+// const app = require("express")();
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// const bodyParser = require("express").json;
+// const port = process.env.PORT || 3000;
+// const User = require("./models/User");
+// const cors = require("cors");
+// const pageobj = require("./routes/pageapis");
+// const mailer = require("express-mailer");
+// const gamesRouter = require("./routes/gameslist");
+// // const db=require("./config/db");
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io=new Server(server);
+
+
+
+
+
+// app.use(cors());
+// mongoose.set("strictQuery", false);
+// // const uri=process.env.MONGO_URI;
+// // const client = new MongoClient(uri);
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// app.use(bodyParser());
+
+// mailer.extend(app, {
+//   from: "no-reply@example.com",
+//   host: "smtp.gmail.com",
+//   secureConnection: true,
+//   port: 465,
+//   transportMethod: "SMTP",
+//   auth: {
+//     user: "gamestore.app.mk@gmail.com",
+//     pass: "zgqfdmkkblmgiuqd",
+//   },
+// });
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect',()=>{
+
+//     console.log('A user disconnected');
+//   })
+// });
+// app.set("email", "/views");
+// app.set("view engine", "jade");
+// app.get("/", async (req, res) => {
+//   res.send("asdfffffffff");
+// });
+// app.use("/games", gamesRouter);
+// app.post("/sendSignupMail", async (req, res, next) => {
+//   let email = req.body.email;
+
+//   app.mailer.send(
+//     "email",
+//     {
+//       to: email, // REQUIRED. This can be a comma delimited string just like a normal email to field.
+//       subject: "Test Email", // REQUIRED.
+//       name:req.body.name,
+//       password:req.body.password,
+//       otherProperty: "heloooooooooooaaaaaaaaaaaaaaaaaaaaaaaaaaa", // All additional properties are also passed to the template as local variables.
+//     },
+//     function (err) {
+//       if (err) {
+//         // handle error
+//         // console.log(err);
+//         res.json("There was an error sending the email");
+//         // res.send('There was an error sending the email');
+//         return;
+//       }
+//       res.json("Email Sent");
+//       console.log(
+//         "adaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa======>>>>>>>>>>>>>>>>>>>>>>>>.",
+//         email
+//       );
+//     }
+//   );
+
+//   //res.send("emailnotyet")
+// });
+
+// app.use("/page", pageobj.Router);
+// app.post("/any", async (req, res) => {
+//   // console.log("hitterdddd seccess",req.ip);
+
+//   console.log("req==============ip", req.ip);
+//   console.log("res========ip", res.ip);
+//   // res.send("AA")
+
+//   let user = new User({
+//     name: req.body.name,
+//     email: req.body.email,
+//   });
+//   await user.save();
+
+//   // let useq=await User.insertOne({
+//   //     name:req.body.name,
+//   //     email:"sec"
+//   // })
+//   // res.send(useq,use)
+//   res.json({ message: "seccess", data: user });
+// });
+// // db.connectDB()
+// // app.listen(port,()=>{
+// //     console.log(`alive at ${port}`);
+// // })
+// connectDB().then(() => {
+//   app.listen(port, () => {
+//     console.log("listening for requests",port);
+//   });
+// });
+// // connectDB().then(() => {
+// //   app.listen(3001, () => {
+// //     console.log("listening for requests");
+// //   });
+// // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////before socket///////
+
+
+
+
+
+
+
 // const { MongoClient } = require("mongodb");
 require("./routes/socketapi");
 const app = require("express")();
