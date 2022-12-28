bcc3e3b58c46b29589524cd3ed2263f9e4208fbd
omG11111 <omgumber01@gmail.com>
2022-12-28 22:41:51 +0530

    try

--- routes/socketapi.js
+++ routes/socketapi.js
@@ -54,8 +54,13 @@ const io = new Server(server,{cors: {
 }});
 const socketport =process.env.SOCKET_PORT || 3020;
 const path =require("path");
+const socketname=io.of('/socket');
 //or
 //io=require("socket.io")(http)
+socketname.use((socket, next) => {
+  // ensure the socket has access to the "users" namespace, and then
+  next();
+});
 
 app2.get('/', (req, res) => {
     console.log("asdasd");
@@ -63,7 +68,7 @@ app2.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-io.on('connection', (socket) => {
+socketname.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect',()=>{
 
@@ -80,12 +85,47 @@ io.on('connection', (socket) => {
   })
   socket.join("room-"+"12")
   socket.join("room-"+"1")
-  io.sockets.to("room-"+"1").emit('connectToRoom', "You are in room no.1 "+romm+"1"+"ID"+socket.id);
+  socket.to("room-"+"1").emit('connectToRoom', "You are in room no.1 "+romm+"1"+"ID"+socket.id);
 
-  io.sockets.to("room-"+"12").emit('connectToRoom', "You are in room no.12 "+romm+"1"+"ID"+socket.id);
+  socket.to("room-"+"12").emit('connectToRoom', "You are in room no.12 "+romm+"1"+"ID"+socket.id);
 });
 
 server.listen(socketport, () => {
   console.log(`listening on *:${socketport}`);
 });
 module.exports=Router
+
+
+
+// app2.get('/', (req, res) => {
+//     console.log("asdasd");
+//     console.log(path.join(__dirname, '../index.html'));
+//   res.sendFile(path.join(__dirname, '../index.html'));
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect',()=>{
+
+//     console.log('A user disconnected');
+//   });
+//   socket.on("message",(ms)=>{
+//     console.log("clint to server",ms);
+
+//   })
+//   let romm="not";
+//   socket.adapter.on("create-room",(room)=>{
+//     console.log(`room ${room} was created`);
+//     romm=room
+//   })
+//   socket.join("room-"+"12")
+//   socket.join("room-"+"1")
+//   io.sockets.to("room-"+"1").emit('connectToRoom', "You are in room no.1 "+romm+"1"+"ID"+socket.id);
+
+//   io.sockets.to("room-"+"12").emit('connectToRoom', "You are in room no.12 "+romm+"1"+"ID"+socket.id);
+// });
+
+// server.listen(socketport, () => {
+//   console.log(`listening on *:${socketport}`);
+// });
+// module.exports=Router
